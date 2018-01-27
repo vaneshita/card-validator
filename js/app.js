786e5881a13468f746778e42f55da3b27349eb84
vaneshita <144995@unsaac.edu.pe>
2018-01-27 16:01:08 -0500

    modificando app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
+// const form = document.querySelector('form');
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   if (validateCardDetails(form)) {
+//     console.log('datos válido... enviar...');
+//   } else {
+//     console.log('datos inválidos');
+//   }
+// });
 var $buttonNext = $('#next');
 var $inputCard = $('#cn');
+function encontrar(array, n) {
+  var encontro = false;
+  // your code here
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === n) {
+      var encontro = true;
+      return encontro;
+    }
+  }
+}
 $inputCard.on('input', function(event) {
   event.preventDefault();
   var nroinput = $inputCard.val();
@@ -10,4 +29,12 @@ $inputCard.on('input', function(event) {
     $(location).attr('href', 'views/datosUsuario.html');
   });
 });
-console.log(localStorage.input);
+console.log(data);
+console.log(data[localStorage.input]);
+console.log(Object.keys(data));
+var busquedaTarjeta = encontrar(Object.keys(data), localStorage.input);
+if (busquedaTarjeta) {
+  $('#nroTarjeta').text(localStorage.input);
+} else {
+  alert('La basre de datos se esta actualizand. Gracias por su comprension');
+}
