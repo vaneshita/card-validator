071a11eaa062e1840131b0587042576e4ce291a5
yoli21 <y_venturchz@hotmail.com>
2018-02-01 13:38:44 -0500

    subiendo arreglos

--- public/js/data.js
+++ public/js/data.js
@@ -1,10 +1,10 @@
 var data = {
-  '4544164785372342': ['vanessa mendoza inoñan', '23/09/2019', '223456', 'https://www.hobbyconsolas.com/sites/hobbyconsolas.com/public/styles/comment_user_icon_90x90/public/pictures/2010/09/picture-18383-a0d8630b43dd105b411b284321451ca1.jpg?itok=21UtLnX2'],
-  '45441647853723': ['juan llontop dominguez', '10/12/2019', '223457', 'https://www.hobbyconsolas.com/sites/hobbyconsolas.com/public/styles/comment_user_icon_90x90/public/pictures/2010/09/picture-18383-a0d8630b43dd105b411b284321451ca1.jpg?itok=21UtLnX2'],
-  '4544164785372375': ['valeria mendez retrina', '03/10/2020', '223458', 'https://www.hobbyconsolas.com/sites/hobbyconsolas.com/public/styles/comment_user_icon_90x90/public/pictures/2010/09/picture-18383-a0d8630b43dd105b411b284321451ca1.jpg?itok=21UtLnX2'],
-  '4544164785372345': ['betty gomez frida', '14/03/2019', '223459', 'https://www.hobbyconsolas.com/sites/hobbyconsolas.com/public/styles/comment_user_icon_90x90/public/pictures/2010/09/picture-18383-a0d8630b43dd105b411b284321451ca1.jpg?itok=21UtLnX2'],
+  '4544164785372342': ['hugo gomez yuma', '23/09/2019', '223456', 'http://0.gravatar.com/avatar/904679afac17ff0b334caaf0328d28f1?s=90&d=mm&r=g'],
+  '45441647853723': ['juan llontop dominguez', '10/12/2019', '223457', 'http://1.gravatar.com/avatar/d24ef6129c1ad18e02f589ca2e5885e8?s=90&d=mm&r=g'],
+  '4544164785372375': ['valeria mendez retrina', '03/10/2020', '223458', 'http://www.adaraestetica.com.ar/images/default/noticias/piel-joven66.jpg'],
+  '4544164785372345': ['betty gomez frida', '14/03/2019', '223459', 'http://2.gravatar.com/avatar/5cb34fe41df09b4991d16586d163a3e6?s=90&d=mm&r=g'],
   '4544164785372326': ['gabriela socorro trinita', '23/05/2018', '223460', 'https://www.hobbyconsolas.com/sites/hobbyconsolas.com/public/styles/comment_user_icon_90x90/public/pictures/2010/09/picture-18383-a0d8630b43dd105b411b284321451ca1.jpg?itok=21UtLnX2'],
-  '4545164875372342': ['dayci berrocal gonzales', '15/06/2018', '223463', 'https://www.hobbyconsolas.com/sites/hobbyconsolas.com/public/styles/comment_user_icon_90x90/public/pictures/2010/09/picture-18383-a0d8630b43dd105b411b284321451ca1.jpg?itok=21UtLnX2'],
-  '4544164785372347': ['hugo gomez yuma', '23/03/24', '223461', 'https://www.hobbyconsolas.com/sites/hobbyconsolas.com/public/styles/comment_user_icon_90x90/public/pictures/2010/09/picture-18383-a0d8630b43dd105b411b284321451ca1.jpg?itok=21UtLnX2'],
-  '4454416478537555': ['humberto trello huamani', '23/10/20', '223462', 'https://www.hobbyconsolas.com/sites/hobbyconsolas.com/public/styles/comment_user_icon_90x90/public/pictures/2010/09/picture-18383-a0d8630b43dd105b411b284321451ca1.jpg?itok=21UtLnX2'],
+  '4545164875372342': ['dayci berrocal gonzales', '15/06/2018', '223463', 'http://www.asuo.org.uy/congreso2016/invitados_nacionales/dra.c.cessio.jpg'],
+  '4544164785372347': ['hugo gomez yuma', '23/03/24', '223461', 'http://1.gravatar.com/avatar/d24ef6129c1ad18e02f589ca2e5885e8?s=90&d=mm&r=g'],
+  '4454416478537555': ['humberto trello huamani', '23/10/20', '223462', 'http://0.gravatar.com/avatar/904679afac17ff0b334caaf0328d28f1?s=90&d=mm&r=g'],
 };
